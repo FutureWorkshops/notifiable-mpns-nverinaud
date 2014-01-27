d93975f4f309143149b6e6f7d0c03b2af31303b4
Matt Brooke-Smith <matt@futureworkshops.com>
2014-01-27 00:11:30 +0000

    Add single_notifier_spec.rb

---
+++ spec/single_notifier_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Notifiable::Mpns::Nverinaud::SingleNotifier do
+  
+  let(:m) { Notifiable::Mpns::Nverinaud::SingleNotifier.new }
+  let(:n) { Notifiable::Notification.create(:message => "Test message") }
+  let(:d) { Notifiable::DeviceToken.create(:token => "http://db3.notify.live.net/throttledthirdparty/01.00/123456789123456798", :provider => :mpns) }
+  let(:u) { User.new(d) }
+  
+  it "sends a single mpns notification" do    
+    stub_request(:post, d.token)
+         
+    m.send_notification(n, d)
+    
+    Notifiable::Notification.count.should == 1
+  end
+
+  
+end
