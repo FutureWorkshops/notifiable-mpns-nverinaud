97e7704865d1acfd20b3306acb85c9c8dab2bb9f
Matt Brooke-Smith <matt@futureworkshops.com>
2014-02-13 18:18:59 +0300

    Update single notifier

--- lib/notifiable/mpns/nverinaud/single_notifier.rb
+++ lib/notifiable/mpns/nverinaud/single_notifier.rb
@@ -12,16 +12,19 @@ module Notifiable
             data = {title: n.title, content: n.message, params: n.params}    
             
             response = MicrosoftPushNotificationService.send_notification device_token.token, :toast, data
+            response_code = response.code.to_i
             
-            case response.code.to_i
-            when 200              
-              processed(n, device_token)
+            case response_code
+            when 200
             when 404
               Rails.logger.info "De-registering device token: #{device_token.id}"
               device_token.update_attribute('is_valid', false)
             else
               Rails.logger.error "Error sending notification: #{response.code}"              
             end
+            
+            processed(n, device_token, response_code)
+            
       		end
   		end
     end
